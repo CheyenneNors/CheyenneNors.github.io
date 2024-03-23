0907763d336affe74669a4e52371f83bd04c3e50
CheyenneNors <156718436+CheyenneNors@users.noreply.github.com>
2024-03-22 22:10:50 -0400

    finished javascript file

--- Assignment 4/Part 4/main-finished.js
+++ Assignment 4/Part 4/main-finished.js
@@ -1,11 +1,4 @@
-/*
-Title: main.js
-Author: Cheyenne Norsworthy
-Date: 22 March 2024
-Purpose: to get some practice with using objects and object-oriented 
-techniques in a real-world context.
-*/
-// setup canvas
+// set up canvas
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -19,7 +12,7 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
+// function to generate random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
@@ -40,16 +33,15 @@ class Ball {
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
-
   }
-  
+
   update() {
     if (this.x + this.size >= width) {
-      this.velX = Math.abs(this.velX);
+      this.velX = -Math.abs(this.velX);
     }
 
     if (this.x - this.size <= 0) {
-      this.velX = -Math.abs(this.velX);
+      this.velX = Math.abs(this.velX);
     }
 
     if (this.y + this.size >= height) {
@@ -66,10 +58,10 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if(!(this === ball)) {
+      if (!(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
-        const distance = Math.sqrt( dx * dx + dy * dy);
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
@@ -77,16 +69,17 @@ class Ball {
       }
     }
   }
-
 }
-  
+
 const balls = [];
 
 while (balls.length < 25) {
-  const size = random (10, 20);
-  const ball = new ball(
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
-    random(0 +size, height - size),
+    random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
     randomRGB(),
