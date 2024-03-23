3f39d612492d67751481801f835ed44cb7135abe
CheyenneNors <156718436+CheyenneNors@users.noreply.github.com>
2024-03-22 20:05:51 -0400

    added initial variables and functions. Added raw text string to javascript file.

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -5,3 +5,17 @@
 	Description: create a javascript file to turn the html and raw text file into a working program
 */ 
 
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
